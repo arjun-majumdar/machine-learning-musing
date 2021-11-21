ff266baf0667bc035a2f08580c11144f97096603
Arjun Majumdar <arjun.majumdar@aol.com>
2021-11-21 23:22:58 +0530

    XGBoost GPU classification example
    with sklearn dataset

--- xgboost_gpu_example.py
+++ xgboost_gpu_example.py
@@ -13,6 +13,10 @@ import seaborn as sns
 """
 GPU Accelerated XGBoost
 
+To install GPU enabled xgboost package, the following command was used within an anaconda env-
+pip install xgboost 
+
+
 Refer-
 https://xgboost.readthedocs.io/en/latest/gpu/index.html
 https://github.com/dmlc/xgboost/blob/master/demo/gpu_acceleration/cover_type.py
@@ -60,9 +64,10 @@ params = {
     # Specify number of multi-classes-
     'num_class': 8,
     # Use GPU accelerated algorithm-
-    # 'tree_method': 'gpu_hist'
+    'tree_method': 'gpu_hist',
     # Use CPU accelerated algorithm-
-    'tree_method': 'hist'
+    # 'tree_method': 'hist',
+    'eval_metric': 'mlogloss'
     }
 
 # NOTE:
@@ -75,8 +80,8 @@ dval = xgb.DMatrix(data = X_test, label = y_test)
 
 
 # Python3 dict to contain accuracy results during training-
-# gpu_acc = {}
-cpu_acc = {}
+gpu_acc = {}
+# cpu_acc = {}
 
 start = time.time()
 
@@ -85,17 +90,18 @@ xgb_clf = xgb.train(
     params = params, dtrain = dtrain,
     num_boost_round = num_boosting_rounds,
     evals = [(dval, 'val')],
-    # evals_result = gpu_acc
-    evals_result = cpu_acc
+    evals_result = gpu_acc
+    # evals_result = cpu_acc
     )
 
 end = time.time()
-print(f"XGBoost CPU training time = {end - start} seconds")
+print(f"XGBoost GPU training time = {end - start} seconds")
 # XGBoost CPU training time = 432.7983458042145 seconds
+# XGBoost GPU training time = 221.20399975776672 seconds
 
 
 # Visualize training-
-plt.plot(cpu_acc['val']['mlogloss'])
+plt.plot(gpu_acc['val']['mlogloss'])
 plt.xlabel("num boosting rounds")
 plt.ylabel("mlogloss")
 plt.title("XGBoost training Visualization")
@@ -110,9 +116,23 @@ acc = accuracy_score(y_test, y_pred)
 prec = precision_score(y_test, y_pred, average = 'macro')
 rec = recall_score(y_test, y_pred, average = 'macro')
 
-print("\nTrained XGBoost's val metrics are:")
+print("\nTrained (default) XGBoost's val metrics are:")
 print(f"accuracy = {acc * 100:.2f}%, precision = {prec * 100:.2f}%"
 f" & recall = {rec * 100:.2f}%")
+# Trained (default) XGBoost's val metrics are:
+# accuracy = 96.83%, precision = 95.04% & recall = 93.62%
+
+print(f"Confusion matrix:\n{confusion_matrix(y_true = y_test, y_pred = y_pred)}")
+'''
+Confusion matrix:
+[[51173  1654     1     0    21     4   107]
+ [ 1373 69145    81     0   153    51    22]
+ [    0    98  8646    35    12   148     0]
+ [    0     0    59   595     0    33     0]
+ [   16   259    15     0  2077     5     1]
+ [    5    79   177    21     3  4057     0]
+ [  137    27     0     0     1     0  4962]]
+'''
 
 
 # Most of the parameters are defaults. Consider hyper-parameter tuning to optimizie performance
